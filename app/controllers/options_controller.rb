e317db4702a375e972dcc649fac4d8bf8dd3e7e1
Aboorva1 <abookannan567@gmail.com>
2022-11-08 16:02:25 +0530

    Falsifying other options when one option is selected true

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,6 +1,7 @@
 class OptionsController < ApplicationController
 
   before_action :set_option, only: [:edit, :update, :destroy]  
+  before_action :falsify_all_others, only:[:create, :edit, :update]
 
   def index
     @options = Option.all
@@ -25,10 +26,11 @@ class OptionsController < ApplicationController
   end
 
   def edit
-   
+    
   end
 
   def update
+    
     if (@option.update(option_params))
       redirect_to quiz_question_path(@quiz, @question)
    else
@@ -41,12 +43,18 @@ class OptionsController < ApplicationController
     redirect_to quiz_question_path(@quiz, @question)
   end
 
+
   private
 
   def set_option
     @quiz = Quiz.find(params[:quiz_id])
     @question = Question.find(params[:question_id])
     @option = Option.find(params[:id])
+  end
+
+  def falsify_all_others
+    falsify_others = Option.where("question_id = ? AND id NOT IN (?)" , params[:question_id], @option.id).update_all(:correct_key => false)
+    @option.correct_key = true if params[:correct_key]
   end
 
   def option_params
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,13 +1,7 @@
 class Option < ApplicationRecord
     belongs_to :question
     validates :option1, presence: true
-    has_many :user_answers, dependent: :destroy
-    before_save :falsify_all_others
-    def falsify_all_others
-      if correct_key
-        self.class.where('id != ?', self.id).where('correct_key').update_all(:correct_key => true)
-      end
-    end   
+    has_many :user_answers, dependent: :destroy  
 end
 
 
