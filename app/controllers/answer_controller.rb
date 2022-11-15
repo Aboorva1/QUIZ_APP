e8a262e8c8f715dacfce6971d18548be7e4ccb69
Aboorva1 <abookannan567@gmail.com>
2022-11-15 09:54:47 +0530

    Added validation logic so that user can submit quiz only after answering all questions

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,30 +1,28 @@
 class AnswerController < ApplicationController
   
   $score = 0 
-  $name = ""
-
   def index
     
   end
 
-
   def create_answers
-      if params[:answers].present?
-      $score = 0 
-      params[:answers].to_enum.to_h.each do |key, value|
+    questions_count = Question.where(quiz_id: params[:quiz_id]).count 
+      if params[:answers].present? && questions_count == params[:answers].to_enum.to_h.count
+        $score = 0 
+        params[:answers].to_enum.to_h.each do |key, value|
 
-      @user_answer = UserAnswer.new
-      @user_answer.quiz_id = params[:quiz_id]
-      @user_answer.user_id = current_user.id
-      @user_answer.question_id = key
-      @user_answer.user_key = Option.find_by(option1: value).correct_key
-      @user_answer.option_id = Option.find_by(option1: value).id
-      if @user_answer.user_key == 't'
-        $score += 1 
-      end
-      @user_answer.save
-      
-      end
+        @user_answer = UserAnswer.new
+        @user_answer.quiz_id = params[:quiz_id]
+        @user_answer.user_id = current_user.id
+        @user_answer.question_id = key
+        @user_answer.user_key = Option.find_by(option1: value).correct_key
+        @user_answer.option_id = Option.find_by(option1: value).id
+        if @user_answer.user_key == 't'
+          $score += 1 
+        end
+        @user_answer.save
+        end
+
       @user_quiz = UserQuiz.new
       @user_quiz.quiz_id = params[:quiz_id]
       @user_quiz.user_id = current_user.id
@@ -33,16 +31,15 @@ class AnswerController < ApplicationController
       @user_quiz.name = current_user.name
       @user_quiz.save
       redirect_to answer_result_path
-  else
-    redirect_back(fallback_location: root_path)
-  end
+      else
+        flash[:error] = "Answer all questions"
+        redirect_back(fallback_location: root_path)
+      end
   end
 
 
   def myquiz
-    #@user_quizzes = UserQuiz.search(params[:search])
-    @user_quizzes = UserQuiz.where(user_id: current_user.id).search(params[:search])
-   
+    @user_quizzes = UserQuiz.where(user_id: current_user.id).search(params[:search]) 
   end
 
   def user_answer_params
