6ca6a237c2c426343a408e7ec8b827789f8a6b27
Aboorva1 <abookannan567@gmail.com>
2022-11-15 10:01:12 +0530

    Edit feature for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
 	belongs_to :quiz
     validates :body, presence: true
     has_many :options, dependent: :destroy
-    accepts_nested_attributes_for :options, allow_destroy: true, reject_if: lambda {|attributes| attributes['option1'].blank?}
+    accepts_nested_attributes_for :options, allow_destroy: true
     has_many :user_answers, dependent: :destroy
     
 end
