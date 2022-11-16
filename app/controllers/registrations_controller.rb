92a479057d32c690cbc132dffe40e524bdd3844e
Aboorva1 <abookannan567@gmail.com>
2022-11-16 09:40:34 +0530

    Active storage installed, User can upload profile image

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
 
 
 	def sign_up_params
-		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :image)
 	end
 
 	def account_update_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :quizzes
   has_many :user_answers
+  has_one_attached :image, :dependent => :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
