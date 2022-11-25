de73279cd362d3f214bbc15c59837d48438bd1b9
Aboorva1 <abookannan567@gmail.com>
2022-11-25 15:05:30 +0530

    Modified validation for adding exactly 4 options and one correct answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,16 +6,5 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :options, allow_destroy: true
 
   validates :body, presence: true
-  validate :options_count
   
-  private 
-
-  def options_count   
-    unless options.count == 4
-      errors.add(:base, "Question should have exactly 4 options")
-    end 
-    unless Option.where(question_id: self.id, is_correct_answer: "true").count == 1
-      errors.add(:base, "Only one option can be set true")
-    end
-  end
 end
