21cf560722ebf6212adc72c28285df48527c937e
Aboorva1 <abookannan567@gmail.com>
2022-11-30 13:50:52 +0530

    View to display questions one by one

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post 'user_quizzes/user_quiz_page'
+  post 'user_quizzes/quiz_user_page'
+  post 'user_quizzes/submit_quiz'
   devise_for :users, controllers: { registrations: "registrations" }
   
   root "quizzes#index"
