536335895e5c9c7be2d8015f381212f250bbdac8
Aboorva1 <abookannan567@gmail.com>
2022-12-07 09:32:29 +0530

    Based on auto_save value made changes in controller to store useranswers and user_quiz for all scenarios

--- app/controllers/user_quizzes_controller.rb
+++ app/controllers/user_quizzes_controller.rb
@@ -1,76 +1,62 @@
 class UserQuizzesController < ApplicationController
-  before_action :set_page, only: [:show, :quiz_user_page]
+  before_action :set_page, only: [:show]
 
   def show
     questions_per_page = 1
+    if @page == 0
+      session[:start_time] = Time.now
+      session[:quiz_id] = params[:id]
+    end
     @quiz = Quiz.find(params[:id])
     @question_count = Question.where(quiz_id: params[:id]).count
     @question = Question.where(quiz_id: params[:id]).offset(questions_per_page * @page).limit(questions_per_page)
   end
 
   def save_answer
-    if params[:answers].present?
-      quiz_id = save_user_answer(params[:answers])
-      page = params[:page].to_i + 1
-      redirect_to user_quiz_path(page: page, id: quiz_id)
+    if (params[:auto_save] == "false")
+      if (params[:answers].present?)
+        quiz_id = save_user_answer(params[:answers])
+        page = params[:page].to_i + 1
+        redirect_to user_quiz_path(page: page, id: quiz_id)
+      else
+        flash[:error] = "Please select one option"
+        redirect_back(fallback_location: root_path)
+      end
     else
-      flash[:error] = "Please select one option"
-      redirect_back(fallback_location: root_path)
+      if (params[:answers].present?)
+        quiz_id = save_user_answer(params[:answers])
+        answers_count = (params[:page].to_i) + 1
+        save_user_quiz(answers_count)
+      else
+        answers_count = (params[:page].to_i)
+        save_user_quiz(answers_count)
+      end
     end
-    
   end
+
 
   def submit_quiz
-    if params[:answers].present?
-      quiz_id = save_user_answer(params[:answers])
-      question_scores = []
-      questions_count = Question.where(quiz_id: quiz_id).count
-      UserAnswer.where(quiz_id: quiz_id).last(questions_count).each do |user_answer|
-        question_scores << user_answer.score
+    if (params[:auto_save] == "false")
+      if (params[:answers].present?)
+        quiz_id = save_user_answer(params[:answers])
+        answers_count = Question.where(quiz_id: quiz_id).count
+        save_user_quiz(answers_count)
+      else
+        flash[:error] = "Please select one option"
+        redirect_back(fallback_location: root_path)
       end
-      @user_quiz = UserQuiz.new(
-        quiz_id: quiz_id,
-        user_id: current_user.id,
-        score: (question_scores.sum) * 10
-      )
-      @user_quiz.save
-      redirect_to result_user_quizzes_path(:id => quiz_id, :quiz_score => @user_quiz.score, :questions_count => questions_count)
     else
-      flash[:error] = "Please select one option"
-      redirect_back(fallback_location: root_path)
+      if params[:answers].present? 
+        quiz_id = save_user_answer(params[:answers])
+        answers_count = Question.where(quiz_id: quiz_id).count
+        save_user_quiz(answers_count)
+      else
+        answers_count = (params[:page].to_i)
+      end
     end
   end
 
-  def user_quiz_page
-    questions_count = Question.where(quiz_id: params[:quiz_id]).count 
-    if params[:answers].present? && questions_count == params[:answers].to_enum.to_h.count
-      score = 0 
-      params[:answers].to_enum.to_h.each do |key, value|
-        @user_answer = UserAnswer.new(
-          quiz_id: params[:quiz_id],
-          user_id: current_user.id,
-          question_id: key,
-          user_correct_answer: Option.find_by(choice: value, question_id: key).is_correct_answer,
-          option_id: Option.find_by(choice: value).id
-        )
-        if @user_answer.user_correct_answer == 't'
-          score += 1 
-        end
-        @user_answer.save
-      end
-      @user_quiz = UserQuiz.new(
-        quiz_id: params[:quiz_id],
-        user_id: current_user.id,
-        score: score * 10
-      )
-      @user_quiz.save
-      redirect_to result_user_quizzes_path(:id => params[:quiz_id], :score => @user_quiz.score, :questions_count => questions_count)
-    else
-      flash[:error] = "Answer all questions"
-      redirect_back(fallback_location: root_path)
-    end
-  end
-        
+
   def my_quiz
     @user_quizzes = UserQuiz.where(user_id: current_user.id) 
   end 
@@ -80,15 +66,17 @@ class UserQuizzesController < ApplicationController
 
   def save_user_answer(answer)
     score = 0
+    end_time = Time.now
     quiz_id = Question.find_by(id: answer.keys.first).quiz_id
     answer.to_enum.to_h.each do |key, value|
       @user_answer = UserAnswer.new(
         quiz_id: quiz_id,
         user_id: current_user.id,
-          question_id: key,
-          user_correct_answer: Option.find_by(choice: value, question_id: key).is_correct_answer,
-          option_id: Option.find_by(choice: value).id
-        )
+        question_id: key,
+        user_correct_answer: Option.find_by(choice: value, question_id: key).is_correct_answer,
+        option_id: Option.find_by(choice: value).id,
+      )
+
     end
     if @user_answer.user_correct_answer == 't'
       score += 1
@@ -96,6 +84,22 @@ class UserQuizzesController < ApplicationController
     @user_answer.score = score
     @user_answer.save
     return quiz_id
+  end
+
+  def save_user_quiz(answers_count)
+    questions_count = Question.where(quiz_id: session[:quiz_id]).count
+    question_scores = []
+    UserAnswer.where(quiz_id: session[:quiz_id]).last(answers_count).each do |user_answer|
+      question_scores << user_answer.score
+    end
+    duration = (Time.now) - (session[:start_time].to_datetime)
+    @user_quiz = UserQuiz.new(
+      quiz_id: session[:quiz_id],
+      user_id: current_user.id,
+      score: (question_scores.sum) * 10
+    )
+    @user_quiz.save!
+    redirect_to result_user_quizzes_path(:id => session[:quiz_id], :quiz_score => @user_quiz.score, :questions_count => questions_count, :answers_count =>answers_count)
   end
 
   def set_page
