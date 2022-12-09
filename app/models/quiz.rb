8de882271aa7f3e1018577529d18b4e0176a28ad
Aboorva1 <abookannan567@gmail.com>
2022-12-09 19:15:43 +0530

    Fixed the changes suggested

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,7 @@
 class Quiz < ApplicationRecord
     belongs_to :category
     belongs_to :user, optional: true
-    validates :title, presence: true
+    validates :title, presence: true, length: { in: 8..20 }
     has_many :questions, dependent: :destroy
     has_many :user_answers, dependent: :destroy
 end
