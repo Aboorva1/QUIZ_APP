480e1803b3c58d40a2757cd9d747c8af76f2820e
Aboorva1 <abookannan567@gmail.com>
2022-12-26 13:10:23 +0530

    Added a user board to admin side where user can view all users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
+  
   def index
     @users = User.where.not(name: "Admin")
   end
 
-  def userboard
+  def user_board
     @user_quizzes = UserQuiz.where(user_id: params[:id])
     @user = User.find_by(id: params[:id])
+    add_breadcrumb(@user.name)
   end
 
 end
